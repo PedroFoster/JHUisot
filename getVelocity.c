ace073fc31cd009ab5a206a5eeee530dfbbf0a61
Foster <pppfoster@gmail.com>
2017-08-11 16:21:14 -0300

    A lot of fixed erros in loops

--- getVelocity.c
+++ getVelocity.c
@@ -44,15 +44,16 @@ int main(int argc,char** argv){
 
  for(time0=0.25F; time0 < 2.1F; time0=time0+0.25F){ 
 
-//   for(w=0;w<N;w+=1){
-//    for(i=0;i<N;i+=1){
-//     for(j=0;j<N;j+=1){
-//  	  position[j][0] = (float)(j*2)*M_PI/((float)N);
-//  	  position[j][1] = (float)(i*2)*M_PI/((float)N);
-//  	  position[j][2] = (float)(w*2)*M_PI/((float)N);
-//    }
-
-
+   for(w=0;w<N;w+=1){
+    for(i=0;i<N;i+=1){
+     for(j=0;j<N;j+=1){
+  	  position[i*N+j][0] = (float)(j*2)*M_PI/((float)N);
+  	  position[i*N+j][1] = (float)(i*2)*M_PI/((float)N);
+  	  position[i*N+j][2] = (float)(w*2)*M_PI/((float)N);
+    }
+    }
+    
+/*
    for(w1=0;w1<32;w1+=1){
     for(w=0;w<32;w+=1){
       for(i=0;i<N;i+=1){
@@ -65,15 +66,15 @@ int main(int argc,char** argv){
   	    position[w*N*N+i*N+j][2] = (float)((w1*32+w)*2)*3.141592/((float)N);
       }
      }
-    }    
-
+    } */   
+    printf("i*N+j = %d\n",i*N+j);
 
     //getVelocity (authtoken, dataset, time0, spatialInterp, temporalInterp, N, position, velocity);
 
 //    turblibSetExitOnError(0);
     printf("Calling for the JHTDB...\n");
     step_clock = omp_get_wtime();
-    while (getVelocity (authtoken, dataset, time0, spatialInterp, temporalInterp, 32*N*N, position, velocity) != SOAP_OK) {
+    while (getVelocity (authtoken, dataset, time0, spatialInterp, temporalInterp, N*N, position, velocity) != SOAP_OK) {
       if (attempts++ > 7000) {
         printf("Fatal Error: too many failures\n");
         exit(1);
@@ -85,16 +86,17 @@ int main(int argc,char** argv){
       sleep(60);
     }
 
-    for (j = 0; j < 32*N*N; j++) {
+    for (j = 0; j < N*N; j++) {
       fprintf(fdadosx,"%f\n", velocity[j][0]);
       fprintf(fdadosy,"%f\n", velocity[j][1]);
 //      fprintf(fdadosz,"%f\n", velocity[j][2]);
     }
 
     relatorio = fopen("./data/relatorio.txt","w");
-    fprintf(relatorio,"time =%f, Block %d has been downloaded in %f minutes\n", time0,w1,(step_clock - omp_get_wtime())/60.);
-    printf("Next step, %d block at time %f concluded\n",w1+1,time0);
-    fclose(relatorio);	
+    fprintf(relatorio,"time =%f, plane %d has been downloaded in %f minutes\n", time0,w,(step_clock - omp_get_wtime())/60.);
+//    printf("Next step, %d block at time %f concluded\n",w1+1,time0);
+    printf("Next step, %d plane at time %f concluded\n",w,time0);
+    fclose(relatorio);
     }
 //    }
 //    fprintf(fdados,"\n");
