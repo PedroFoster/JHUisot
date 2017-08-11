d0f4f32d483816ee08143bdda5259fee96427712
Foster <pppfoster@gmail.com>
2017-08-11 12:21:16 -0300

    getVelocity.c: instead of line, get 32 planes at once

--- getVelocity.c
+++ getVelocity.c
@@ -6,7 +6,7 @@
 #include<omp.h>
 
 int main(int argc,char** argv){
-  int i,j,w,N=1024;
+  int i,j,w1,w2,N=1024;
   int attempts = 0;
   char *authtoken = "br.ufrj.if.foster-2377d412";
   char *dataset = "isotropic1024coarse";
@@ -16,8 +16,8 @@ int main(int argc,char** argv){
   float myclock;
   enum SpatialInterpolation spatialInterp = Lag6;
   enum TemporalInterpolation temporalInterp = NoTInt;
-  float (*position)[3] = malloc(N*sizeof(*position));
-  float (*velocity)[3] = malloc(N*sizeof(*velocity));
+  float (*position)[3] = malloc(32*N*N*sizeof(*position));
+  float (*velocity)[3] = malloc(32*N*N*sizeof(*velocity));
 
   FILE *resume = NULL;
   resume = fopen("./data/log_cubointeiro.txt","w");
@@ -26,8 +26,8 @@ int main(int argc,char** argv){
   FILE *fdadosy = NULL;
   FILE *fdadosz = NULL;
   FILE *relatorio = NULL;
-  fdadosx=fopen("./data/vx_long_isot_t25to200_cubointeiro_teste.txt","w");
-  fdadosy=fopen("./data/vy_transv_isot_t25to200_cubointeiro_teste.txt","w");
+  fdadosx=fopen("./data/vx_long_isot_t25to200_.txt","w");
+  fdadosy=fopen("./data/vy_transv_isot_t25to200_.txt","w");
 //  fdadosz=fopen("./data/vz_long_isot_t60_N1024_cubointeiro_900to1023.txt","w");
 
   /* Start clock */
@@ -44,13 +44,23 @@ int main(int argc,char** argv){
 
  for(time0=0.25F; time0 < 2.1F; time0=time0+0.25F){ 
 
-   for(w=0;w<N;w+=1){
-    for(i=0;i<N;i+=1){
-     for(j=0;j<N;j+=1){
-  	  position[j][0] = (float)(j*2)*M_PI/((float)N);
-  	  position[j][1] = (float)(i*2)*M_PI/((float)N);
-  	  position[j][2] = (float)(w*2)*M_PI/((float)N);
-    }
+//   for(w=0;w<N;w+=1){
+//    for(i=0;i<N;i+=1){
+//     for(j=0;j<N;j+=1){
+//  	  position[j][0] = (float)(j*2)*M_PI/((float)N);
+//  	  position[j][1] = (float)(i*2)*M_PI/((float)N);
+//  	  position[j][2] = (float)(w*2)*M_PI/((float)N);
+//    }
+
+
+   for(w1=0;w1<32;w1+=1){
+    for(w=0;w<32;w+1){
+      for(i=0;i<N;i+=1){
+       for(j=0;j<N;j+=1){
+  	    position[w*N*N+i*N+j][0] = (float)(j*2)*M_PI/((float)N);
+  	    position[w*N*N+i*N+j][1] = (float)(i*2)*M_PI/((float)N);
+  	    position[w*N*N+i*N+j][2] = (float)((w1*32+w)*2)*M_PI/((float)N);
+      }
 
 
     //getVelocity (authtoken, dataset, time0, spatialInterp, temporalInterp, N, position, velocity);
@@ -68,7 +78,7 @@ int main(int argc,char** argv){
       sleep(60);
     }
 
-    for (j = 0; j < N; j++) {
+    for (j = 0; j < 32*N*N; j++) {
       fprintf(fdadosx,"%f\n", velocity[j][0]);
       fprintf(fdadosy,"%f\n", velocity[j][1]);
 //      fprintf(fdadosz,"%f\n", velocity[j][2]);
